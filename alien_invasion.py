d1027b5d1cdfee4789be6299dc535c52d011dabd
Katie Vogel <kvog983@gmail.com>
2023-06-17 13:04:08 -0400

    fixed missing tuple parens in set_mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,7 @@ class AlienInvasion:
 
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # setting the background color
