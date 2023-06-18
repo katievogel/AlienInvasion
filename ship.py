3213d4f4f8ae100862dca45a5a26589182e1d7e4
Katie Vogel <kvog983@gmail.com>
2023-06-17 21:16:56 -0400

    ship moves continuously left and right

--- ship.py
+++ ship.py
@@ -19,6 +19,19 @@ class Ship:
         #start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # movement flag: start with a ship that's not moving
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''
+        Update ship's position based on the movement flag
+        '''
+        if self.moving_right: 
+            self.rect.x += 1
+        if self.moving_left: 
+            self.rect.x -= 1
+
     def blitme(self):
         '''
         Draw the ship at its current location
