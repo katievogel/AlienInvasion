ed21771f0f616f7fa3833a1e85af4dffd1b6d8f3
Katie Vogel <kvog983@gmail.com>
2023-06-18 12:52:18 -0400

    adjust ship’s speed, prevent from going off-screen

--- ship.py
+++ ship.py
@@ -11,13 +11,17 @@ class Ship:
         # pygame lets you treat all game elements as rectangles, or 'rects', for efficiency
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and position on screen 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        #start each new ship at the bottom center of the screen
+        # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
 
         # movement flag: start with a ship that's not moving
         self.moving_right = False
@@ -27,10 +31,15 @@ class Ship:
         '''
         Update ship's position based on the movement flag
         '''
-        if self.moving_right: 
-            self.rect.x += 1
-        if self.moving_left: 
-            self.rect.x -= 1
+        # update the ship's x value, not the rect. prevent from going off screen
+        if self.moving_right and self.rect.right <self.screen_rect.right: 
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0: 
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x
+        self.rect.x = self.x
+
 
     def blitme(self):
         '''
