9dd9fc20b7ad53b51d9d58a900a241fb926ddf0a
Katie Vogel <kvog983@gmail.com>
2023-07-01 12:33:08 -0400

    moving fleet left and right based on x axis

--- settings.py
+++ settings.py
@@ -23,3 +23,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right, -1 is left
+        self.fleet_direction = 1
