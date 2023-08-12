2fa9571734c3aa7af94f89786bec7fce671e1213
Katie Vogel <kvog983@gmail.com>
2023-08-12 11:48:43 -0400

    logic and display of scoreboard in upper right corner

--- scoreboard.py
+++ scoreboard.py
@@ -19,6 +19,7 @@ class Scoreboard:
         #prepare the initial score image
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_high_score(self):
         '''
@@ -46,6 +47,18 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_level(self):
+        '''
+        Turn the level into a rendered image
+        '''
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+
+        #Position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def check_high_score(self):
         '''
         Check to see if there is a new high score
@@ -53,6 +66,7 @@ class Scoreboard:
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
+            self.prep_level()
 
     def show_score(self):
         '''
@@ -60,3 +74,4 @@ class Scoreboard:
         '''
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
