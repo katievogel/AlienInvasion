47f2d5be71a8d6b846ff9542879d8e76fc46c993
Katie Vogel <kvog983@gmail.com>
2023-09-10 14:35:01 -0400

    creation of ship lives, images, size scaling

--- ship.py
+++ ship.py
@@ -4,6 +4,8 @@ from settings import Settings
 
 settings = Settings()
 DEFAULT_IMAGE_SIZE = (settings.screen_width * .12, settings.screen_height * .2)
+DEFAULT_LIVES_IMAGE_SIZE = (settings.screen_width * .12, settings.screen_height * .2)
+
 
 class Ship(Sprite):
     '''
@@ -63,4 +65,57 @@ class Ship(Sprite):
         '''
         Draw the ship at its current location
         '''
+        self.screen.blit(self.scaled_image, self.rect)
+
+class ShipLivesGreen(Sprite):
+    '''
+    Class to control the graphical representation of how many lives are left for the player
+    '''
+    def __init__(self, ai_game):
+        '''
+        Initialize the ship and set it starting position
+        '''
+        super().__init__()
+        # pygame lets you treat all game elements as rectangles, or 'rects', for efficiency
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # Load the ship lives green image 
+        self.image = pygame.image.load('images/green-circle.png')
+ 
+        # Scale the image to appropriate size in play area
+        self.scaled_image = pygame.transform.scale(self.image, DEFAULT_LIVES_IMAGE_SIZE)
+        
+        # Load ship lives images position on screen
+        self.rect = self.image.get_rect()
+
+    def blitme(self):
+        self.screen.blit(self.scaled_image, self.rect)
+        
+
+class ShipLivesRed(Sprite):
+    '''
+    Class to control the graphical representation of how many lives are left for the player
+    '''
+    def __init__(self, ai_game):
+        '''
+        Initialize the ship and set it starting position
+        '''
+        super().__init__()
+        # pygame lets you treat all game elements as rectangles, or 'rects', for efficiency
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # Load the ship lives red image 
+        self.image = pygame.image.load('images/red-x.png')
+ 
+        # Scale the image to appropriate size in play area
+        self.scaled_image = pygame.transform.scale(self.image, DEFAULT_LIVES_IMAGE_SIZE)
+        
+        # Load ship lives images position on screen
+        self.rect = self.scaled_image.get_rect()                 
+
+    def blitme(self):
         self.screen.blit(self.scaled_image, self.rect)
